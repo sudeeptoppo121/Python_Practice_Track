869f3ac41e4e5e5e407295a0cf1352365a18de59
Sudeep Toppo <sudeeptoppo121jh.gmail.com>
2026-08-11 23:24:52 +0530

    study hours calculate

---
+++ 11July26/display_study_hours.py
@@ -0,0 +1,9 @@
+def display_study_hours():
+    hours_per_day = int(input())
+    days = int(input())
+    total = hours_per_day * days
+    print(f"Total Study Hours: {total}")
+
+
+# Call the function
+display_study_hours()
